87fb64544dd130ea3c59386e29a08d43ab7875f1
Johannes <office@atelierbuchner.at>
2024-05-15 17:35:56 +0200

    client list

---
+++ sanity/sabocon/schemas/clients.js
@@ -0,0 +1,12 @@
+export default {
+  name: 'clients',
+  type: 'document',
+  title: 'clients',
+  fields: [
+    {
+      title: 'Client List',
+      name: 'clientList',
+      type: 'string',
+    },
+  ],
+}
